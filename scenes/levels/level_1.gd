e38964507679f366be6b6dd0e29e4da505b801e7
Robert Kadak <robert.kadak@gmail.com>
2024-10-13 02:30:52 +0300

    Added dynamic boxes

--- scenes/levels/level_1.gd
+++ scenes/levels/level_1.gd
@@ -3,9 +3,16 @@ extends Node2D
 var elapsedTime : int
 var diffInd : int 
 
+@export var box_region_start: Node2D
+@export var box_region_end: Node2D
+@export var box_region_start2: Node2D
+@export var box_region_end2: Node2D
+@onready var box1 = preload("res://scenes/object.tscn")
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
+	generate_objects(box_region_start, box_region_end, box1)
+	generate_objects(box_region_start2, box_region_end2, box1)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -14,3 +21,16 @@ func _process(delta: float) -> void:
 func is_level_clear():
 	var enemies = get_tree().get_nodes_in_group("Enemy")
 	return len(enemies) == 0
+
+func generate_objects(region_start_node: Node2D, region_end_node: Node2D,
+		object_resource: Resource, count=1):
+	while count != 0:
+		var x = randi_range(region_start_node.position.x, region_end_node.position.x)
+		var y = randi_range(region_start_node.position.y, region_end_node.position.y)
+		
+		var object = object_resource.instantiate()
+		object.position = Vector2(x, y)
+		add_child(object)
+		
+		# Might add some constriction checks
+		count -= 1
