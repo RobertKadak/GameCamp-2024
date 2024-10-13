988932dfc035dec1ed041a37f04688fbc2f6dda1
wa75on <dzapryanov05@gmail.com>
2024-10-13 10:17:26 +0300

    Working spawner, to fix spawn-rate and location later

--- scenes/levels/level_1.gd
+++ scenes/levels/level_1.gd
@@ -1,7 +1,6 @@
 extends Node2D
 
-var elapsedTime : int
-var diffInd : int 
+@onready var enemy = preload("res://scenes/enemy.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -11,6 +10,13 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	pass
 
+
 func is_level_clear():
 	var enemies = get_tree().get_nodes_in_group("Enemy")
 	return len(enemies) == 0
+
+
+func _on_mob_spawn_timeout() -> void:
+	var mob = enemy.instantiate()
+	add_child(mob)
+	mob.target_to_chase  = $Player
