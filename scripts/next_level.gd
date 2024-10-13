575fccf968025848c830f5264561eb5a6ad5dc6d
wa75on <dzapryanov05@gmail.com>
2024-10-13 17:01:40 +0300

    FIXED LVL

--- scripts/next_level.gd
+++ scripts/next_level.gd
@@ -2,17 +2,18 @@ extends Area2D
 
 @onready var level = get_parent()
 
+
 func _process(delta: float) -> void:
-	if monitoring == false and level.is_level_clear():
-		monitoring = true
+	monitoring = true
+	pass
 
 func _on_body_entered(body: Node2D) -> void:
-	if (level.levelTrack == 3):
-		pass
 	if body.is_in_group("Player"):
-		monitoring = false
-		level.get_node("Player").global_position = Vector2.ZERO
-		level.clear()
-		level.generate_objects()
-		level.nextLevel()
+		if(level.levelTrack == 3):
+			pass
+		if(level.is_level_clear()):
+			level.get_node("Player").global_position = Vector2.ZERO
+			level.clear()
+			level.generate_objects()
+			level.nextLevel()
 		pass # level funtion to clear, regenerate level and teleport player
